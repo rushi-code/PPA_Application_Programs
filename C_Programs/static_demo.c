23e39936fed79f568e54db52999d8b6c5cdae893
Rushikesh Ingale <127707145+rushi-code@users.noreply.github.com>
2023-05-07 00:13:26 +0530

    C

---
+++ C_Programs/static_demo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+static int z=15;
+void Demo()
+{
+    static int No = 10;
+    No++;
+
+    printf("%d\n",No);
+}
+
+int main()
+{
+    Demo();
+    Demo();
+    Demo();
+
+    return 0;
+}
